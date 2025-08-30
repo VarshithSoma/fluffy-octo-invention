258c1f66b562b8539074b9a37580e66f9714e0f3
varshithkumarsoma <varshithkumarsoma@gmail.com>
2025-08-30 19:44:27 +0530

    chore: add jwt auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,40 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
-const app = new Hono<{
+interface Env {
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
-}>();
+}
 
-app.get("/hello", async (c) => {
+const app = new Hono<Env>();
+app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-
-  const data = await prisma.user.findMany();
-
-  return c.json(data);
+  try {
+    const body = await c.req.json();
+    const data = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    const token = await sign(
+      {
+        email: data.id,
+        exp: Math.floor(Date.now() / 1000) + 60 * 5,
+      },
+      c.env.JWT_SECRET
+    );
+    return c.json(token);
+  } catch (error) {
+    c.status(411);
+    return c.text("Invalid");
+  }
 });
 
 export default app;
